fd8f3cdee4509f0655d33dc389952d3697681591
Gustavo Zapata <tavordie@hotmail.com>
2019-05-21 16:56:41 +0100

    added Montserrat font

--- app/src/main/java/me/gustavozapata/today/MainActivity.java
+++ app/src/main/java/me/gustavozapata/today/MainActivity.java
@@ -1,7 +1,9 @@
 package me.gustavozapata.today;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= 21){
+            this.getWindow().setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+        }
     }
 }
