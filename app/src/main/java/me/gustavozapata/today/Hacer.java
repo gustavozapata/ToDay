f0da0f69a4618b037f7368ae286ceb9743870e83
gustavozapata <tavordie@hotmail.com>
2019-06-13 17:04:49 +0100

    Delete Hacer.java

--- app/src/main/java/me/gustavozapata/today/Hacer.java
+++
@@ -1,5 +0,0 @@
-package me.gustavozapata.today;
-
-public class Hacer {
-    //TODO: apply runtime color theme switch - https://medium.com/@vinitagrawal91/android-colors-and-multiple-themes-fdfca3f75a15
-}
