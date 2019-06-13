ea2d476df66a62b22f7bac653b61364445399377
Gustavo Zapata <tavordie@hotmail.com>
2019-06-13 17:04:02 +0100

    hacer.java

--- app/src/main/java/me/gustavozapata/today/Hacer.java
+++ app/src/main/java/me/gustavozapata/today/Hacer.java
@@ -1,5 +1,5 @@
 package me.gustavozapata.today;
 
-public class Todo {
+public class Hacer {
     //TODO: apply runtime color theme switch - https://medium.com/@vinitagrawal91/android-colors-and-multiple-themes-fdfca3f75a15
 }
